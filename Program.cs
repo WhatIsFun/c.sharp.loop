6fe1eb48b973efd7f70d91e251d1a19f16d22ee7
WhatIsFun <mohammed110051@gmail.com>
2023-06-02 19:16:01 +0400

    loop test write 0 to 9

--- Program.cs
+++ Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("");
+
         }
     }
 }
