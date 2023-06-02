f5d5a524166cee28f35878310cede6f640accb8d
WhatIsFun <mohammed110051@gmail.com>
2023-06-02 19:40:16 +0400

    Add Star Pattern

--- Program.cs
+++ Program.cs
@@ -2,13 +2,36 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args)    
         {
-            for (int i = 0; i < 10; i++)
+            /*for (int i = 0; i < 10; i++)      //Loop test write 0 to 9
             {
                 Console.WriteLine(i);
+                
             }
-            Console.WriteLine("");
+            Console.WriteLine("");*/
+            string st = "*";
+
+            for (int i = 1; i < 4; i++)
+            {
+                Console.WriteLine("*");
+                st += i;
+                if (i == 4)
+                {
+                    Console.WriteLine(" ");
+                    st += "*";
+
+                    if (i == 1)
+                    {
+                        Console.WriteLine("*****");
+                    }
+
+
+
+                }
+
+
+            }
 
         }
     }
