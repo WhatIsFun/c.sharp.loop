4a85bd937a8248e135f1361ec4c9d6cdb62d115a
WhatIsFun <mohammed110051@gmail.com>
2023-06-05 08:21:11 +0400

    Factorial v1.0

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace c.sharp.loops
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace c.sharp.loops
 {
     internal class Program
     {
@@ -11,120 +13,122 @@
             }
             Console.WriteLine("");*/
 
-            /*for (int i = 1; i <= 5; i++) // Star Square
+/*for (int i = 1; i <= 5; i++) // Star Square
+{
+    Console.Write("*"); // to print *****
+}
+Console.WriteLine();
+for (int i = 1; i <= 3; i++)
+{
+    Console.WriteLine("*   *"); // to print *    *
+}
+
+for (int i = 1; i <= 5; i++)
+{
+    Console.Write("*"); // to print *****
+}*/
+
+//Do - While Loop
+/*int a = 1;
+do
+{
+    Console.WriteLine(a);
+    a++;
+} while (a < 4);
+*/
+
+
+// Odd numbers scanner between two numbers input from user ex: 2, 6
+/*Console.WriteLine("Hello, programmer");
+Console.WriteLine("Enter your first number:");
+int fN = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter your last number:");
+int lN = Convert.ToInt32(Console.ReadLine());
+
+
+for (int i = fN; i <= (lN - i); i++)
+{
+
+    if (i < lN)
+    {
+        Console.WriteLine(i + (i - 1));
+
+    }
+
+}*/
+
+
+
+
+
+
+Console.WriteLine("Hello, programmer");
+Console.WriteLine("Enter your number:");
+int i, factorial = 1;
+int N = Convert.ToInt32(Console.ReadLine());
+for (i = 1; i <= N; i++)
+{
+    factorial *= i;
+
+    //Console.Write(i);
+}
+Console.WriteLine(" factorial of " + N + " is " + factorial);
+
+
+
+
+/*// test Star pattern part 1
+//*
+//**
+//***
+//****
+//*****
+
+for (int i = 1; i <= 6; ++i)
+{
+    for (int j = 1; j <= i; ++j)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+
+    //Part 2  
+    if (i == 6)
+    {
+        //Console.WriteLine("Test");
+
+        for (i = 6; i >= 1; --i)
+        {
+            for (int j = 1; j <= i; ++j)
             {
-                Console.Write("*"); // to print *****
+                Console.Write("*");
+
+                // Part 3 
+                // star line 
+                //******
+                if (i == 1)
+                {
+                    Console.WriteLine("******");
+                }
             }
             Console.WriteLine();
-            for (int i = 1; i <= 3; i++)
-            {
-                Console.WriteLine("*   *"); // to print *    *
-            }
-
-            for (int i = 1; i <= 5; i++)
-            {
-                Console.Write("*"); // to print *****
-            }*/
-
-            //Do - While Loop
-            /*int a = 1;
-            do
-            {
-                Console.WriteLine(a);
-                a++;
-            } while (a < 4);
-            */
-
-            /*Console.WriteLine("Hello, programmer");
-            Console.WriteLine("Enter your first number:");
-            int fN = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter your last number:");
-            int lN = Convert.ToInt32(Console.ReadLine());
-
-
-            for (int i = fN; i <= (lN - i); i++)
-            {
-
-                if (i < lN)
-                {
-                    Console.WriteLine(i + (i - 1));
-
-                }
-
-            }*/
-
-
-
-
-
-
-            Console.WriteLine("Hello, programmer");
-            Console.WriteLine("Enter your number:");
-            int i, factorial = 1, number;
-            int N = Convert.ToInt32(Console.ReadLine());
-            for (i = 1; i <= N; i++)
-            {
-                factorial = N * i;
-
-                //Console.Write(i);
-            }
-            Console.WriteLine(" factoial of " + N + " is " + factorial);
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-            /*// test Star pattern part 1
-            //*
-            //**
-            //***
-            //****
-            //*****
-
-            for (int i = 1; i <= 6; ++i)
-            {
-                for (int j = 1; j <= i; ++j)
-                {
-                    Console.Write("*");
-                }
-                Console.WriteLine();
-
-                //Part 2  
-                if (i == 6)
-                {
-                    //Console.WriteLine("Test");
-
-                    for (i = 6; i >= 1; --i)
-                    {
-                        for (int j = 1; j <= i; ++j)
-                        {
-                            Console.Write("*");
-
-                            // Part 3 
-                            // star line 
-                            //******
-                            if (i == 1)
-                            {
-                                Console.WriteLine("******");
-                            }
-                        }
-                        Console.WriteLine();
-                    }
-                    Console.ReadLine();
-                }
-
-
-            }*/
-
         }
+        Console.ReadLine();
     }
+
+
+}*/
+
+/*Console.WriteLine("Welcome to sum program"); // Sum 
+Console.WriteLine("Enter your number:");
+int n = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine("Sum of the first number "+ n +" is " + sum);*/
+
+}
+}
 }
